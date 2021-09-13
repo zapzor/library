8c296058a92815fc71abcdb935eb2a4411cf8a9f
Zapzor <imp.4xlbm@aleeas.com>
2021-09-13 02:00:13 +0100

    Added modal to add books and ability to remove books

--- library.js
+++ library.js
@@ -1,28 +1,69 @@
 // Vars and containers
 let myLibrary = [];
-const bookDisplay = document.querySelector("#bookDisplay");
+
+const bookDisplay = document.querySelector('.bookDisplay')
+const newBookFormx = document.querySelector('.newBookForm')
+
+const openModalButton = document.querySelector('.modalButton')
+const overlay = document.querySelector('.overlay')
+const modal = document.querySelector('.modal')
+const submitBook = document.querySelector('.submitBook')
+
+openModalButton.addEventListener('click', openModal)
+overlay.addEventListener('click', openModal)
+submitBook.addEventListener('click', submitForm)
+// Hides and unhides modal
+
+function openModal() {
+  modal.classList.toggle('active')
+  overlay.classList.toggle('active')
+}
+
+// Class for creating book objects
 
 class Book {
-  constructor(title, author) {
-  this.title = title;
-  this.author = author;
-  return `Created new book: ${this.title} by ${this.author}`
+  constructor() {
+  this.title = document.getElementById('title').value
+  this.author = document.getElementById('author').value
+  this.pages = document.getElementById('pages').value
+  return console.log(`Created new book: ${this.title} by ${this.author}`)
 }
 
   addBookToLibrary() {
-    myLibrary.push(this.title);
-    return `Added ${this.title} by ${this.author} to the library.`;
+    myLibrary.push(this.title)
+
+
+    let removeBook = document.createElement('button')
+    removeBook.classList.add('removeBook')
+    removeBook.addEventListener('click', () => {
+      bookDisplay.innerHTML = ''
+    })
+    bookDisplay.appendChild(removeBook)
+
+    let book = document.createElement('div')
+    book.classList.add('card')
+    book.textContent = this.title
+    bookDisplay.appendChild(book)
+    return console.log(`Added ${this.title} by ${this.author} to library`)
+  }
+
+
+  displayLibrary() {
+    let text = ""
+    for (let i = 0; i < myLibrary.length; i++) {
+    text += myLibrary[i] + " "
+    }
+    return text
   }
 }
 
-displayLibrary = function () {
-  bookDisplay.textContent = myLibrary
+function removeFromLibrary() {
+  1+1
+
 }
 
-const hobbit = new Book('The Hobbit', 'J.R.R Tolkien');
-const foobar = new Book('foo', 'bar');
-const barfoo = new Book('bar', 'foo');
+function submitForm() {
+  thisBook = new Book()
+  thisBook.addBookToLibrary()
+}
 
-hobbit.addBookToLibrary()
-foobar.addBookToLibrary()
-barfoo.addBookToLibrary()
